42d30a87f8fe465c2f68df062431042c47084d08
Preeti Pawar <preepawar@deloitte.com>
2024-01-23 11:12:49 +0530

    Created a new package and new program under that package.

---
+++ src/test/java/NewPackage/NewPackagePgm1.java
@@ -0,0 +1,12 @@
+package NewPackage;
+
+public class NewPackagePgm1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Create a new package");
+		System.out.println("Create a new class file and write down the program");
+	}
+
+}
