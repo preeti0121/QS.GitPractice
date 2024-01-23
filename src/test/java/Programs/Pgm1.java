063717a4655327c281c408c919071ad60e13d233
Preeti Pawar <preepawar@deloitte.com>
2024-01-23 10:40:20 +0530

    Push the practice project changes

---
+++ src/test/java/Programs/Pgm1.java
@@ -0,0 +1,13 @@
+package Programs;
+
+public class Pgm1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to github");
+		System.out.println("Commit and push the changes to GitHub");
+
+	}
+
+}
