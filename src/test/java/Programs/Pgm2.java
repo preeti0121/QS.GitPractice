73eafa2ff348db701502466f2abc78bb00bd2ed8
Preeti Pawar <preepawar@deloitte.com>
2024-01-23 11:05:43 +0530

    Created new program file

---
+++ src/test/java/Programs/Pgm2.java
@@ -0,0 +1,12 @@
+package Programs;
+
+public class Pgm2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Create new program");
+		System.out.println("Commit and push this program to GitHub");
+
+	}
+
+}
