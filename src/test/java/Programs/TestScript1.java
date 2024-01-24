b77c3ce7c346151af75634dcd60fa9ac35c29442
Preeti Pawar <preepawar@deloitte.com>
2024-01-24 11:53:04 +0530

    TC_001 is committed

---
+++ src/test/java/Programs/TestScript1.java
@@ -0,0 +1,15 @@
+package Programs;
+
+public class TestScript1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Create a test script");
+		System.out.println("Create a local branch and give local branch name as Test case id");
+		System.out.println("Push the changes to local branch");
+		System.out.println("For each and every test script, create a branch and push the changes");
+		System.out.println("Raise a pull request");
+	}
+
+}
