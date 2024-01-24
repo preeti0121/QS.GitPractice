5a11f5114943ed0c9e925981f7507595fd04c8bf
Preeti Pawar <preepawar@deloitte.com>
2024-01-24 10:32:49 +0530

    Object repository package has been added

---
+++ src/main/java/ObjectRepository/AddLocators.java
@@ -0,0 +1,12 @@
+package ObjectRepository;
+
+public class AddLocators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Locators added");
+		System.out.println("Automation tester will have to pull these changes into his/her workspace");
+	}
+
+}
